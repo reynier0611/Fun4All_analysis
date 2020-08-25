f9ce9792296d2cf8da40466974bd498a24d4e89f
Reynier Cruz Torres <reynier@mit.edu>
2020-08-25 09:44:01 -0400

    Added a shell script that shows how to run the code

---
+++ sourceme.sh
@@ -0,0 +1,25 @@
+make clean
+make
+
+# Loop over magnetic field setting
+for B in {"Beast","sPHENIX"}
+do
+	# Loop over pixel size
+	for pix in {20,10}
+	do
+		# Loop over detector configuration
+		for det in {"","_both_GEMs","_both_GEMs_RICH"}
+		do
+			# Do this three times to adjust tables
+			for i in {1..3}
+			do
+				./analysis_resolution 1 1 "skimmed_pi-_det2"$det"_"$pix"x"$pix"_"$B"_FastTrackingEval.root"
+			done
+
+			# Do one last time without updating table
+			./analysis_resolution 1 2 "skimmed_pi-_det2"$det"_"$pix"x"$pix"_"$B"_FastTrackingEval.root"
+		done
+	done
+done
+
+say "All done!"
