48578bdc1014ddd5be1d5b1cd2624b71e908ca34
Reynier Cruz Torres <reynier@mit.edu>
2020-10-27 11:44:25 -0700

    Added codes that create nominal plots

---
+++ analysis/200917_nominal_plots/single_B_field.cxx
@@ -0,0 +1,256 @@
+#include <fstream>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+#include <filesystem>
+
+// Root includes
+#include "TRint.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TStyle.h"
+#include "TF1.h"
+#include "TCanvas.h"
+#include "TLegend.h"
+#include "TVectorT.h"
+#include "TGraphErrors.h"
+#include "TLatex.h"
+#include "TLine.h"
+
+namespace fs = std::filesystem;
+using namespace std;
+
+// Forward-declaring functions
+void prettyTH1F( TH1F * h1 , int color , int marker , float min , float max );
+int idx_from_vector( double value , TVectorT<double> * vec );
+TGraphErrors * graph_from_histo( TH1F * h1 , int color , int marker , float min , float max );
+// ============================================================================================================================================
+int main(int argc, char ** argv) {
+
+#ifdef WITHRINT
+	TRint *myapp = new TRint("RootSession",&argc,argv,NULL,0);
+#else
+	TApplication *myapp = new TApplication("myapp",0,0);
+#endif
+
+	gStyle->SetErrorX(0.0001);
+	// ------------------------------------------------------------------------------
+	// List paths to files that will be loaded
+	TString fnames[] = {
+		"../../output/output_skimmed_pi-_det2_10x10_Beast_FastTrackingEval.root"
+	};
+	// #######################################################################################################################################
+	// YOU SHOULDN'T NEED TO MODIFY ANYTHING IN THE BLOCK OF CODE BELOW AND UNTIL AFTER THE NEXT LINE WITH ###...
+	const int size_loaded = sizeof(fnames)/sizeof(*fnames);
+	// ------------------------------------------------------------------------------
+	// Preparing variables that will later on be filled from root files
+	TVectorT<double> ** TVT_eta_bin = new TVectorT<double>*[size_loaded];
+	TVectorT<double> ** TVT_mom_bin = new TVectorT<double>*[size_loaded];
+	int num_eta_bin[size_loaded] = {0};
+	int num_mom_bin[size_loaded] = {0};
+	// ------------------------------------------------------------------------------
+	// Loading root files and info therein
+	TFile ** Fin = new TFile*[size_loaded];
+
+	TH1F *** h1_dpp_v_p_et_bins = new TH1F ** [size_loaded];
+	TH1F *** h1_dth_v_p_et_bins = new TH1F ** [size_loaded];
+	TH1F *** h1_dph_v_p_et_bins = new TH1F ** [size_loaded];
+	TH1F *** h1_dpp_v_et_p_bins = new TH1F ** [size_loaded];
+	TH1F *** h1_dth_v_et_p_bins = new TH1F ** [size_loaded];
+	TH1F *** h1_dph_v_et_p_bins = new TH1F ** [size_loaded];
+
+	for(int f = 0 ; f < size_loaded ; f++){
+		ifstream fin;
+		fin.open(fnames[f]);
+		if(!fin){ cout << "\033[1;31mCouldn't find input file '" << fnames[f] << "'. Bailing out!\033[0m" << endl; exit(0);}
+		fin.close();
+
+		Fin[f] = new TFile(fnames[f]);
+
+		TVT_eta_bin[f] = (TVectorT<double> *) Fin[f] -> Get("TVT_eta_bin");
+		TVT_mom_bin[f] = (TVectorT<double> *) Fin[f] -> Get("TVT_mom_bin");
+
+		num_eta_bin[f] = (*TVT_eta_bin[f]).GetNoElements()-1;
+		num_mom_bin[f] = (*TVT_mom_bin[f]).GetNoElements()-1;
+
+		h1_dpp_v_p_et_bins[f] = new TH1F * [num_eta_bin[f]];
+		h1_dth_v_p_et_bins[f] = new TH1F * [num_eta_bin[f]];
+		h1_dph_v_p_et_bins[f] = new TH1F * [num_eta_bin[f]];
+		h1_dpp_v_et_p_bins[f] = new TH1F * [num_mom_bin[f]];
+		h1_dth_v_et_p_bins[f] = new TH1F * [num_mom_bin[f]];
+		h1_dph_v_et_p_bins[f] = new TH1F * [num_mom_bin[f]];
+
+		for(int et = 0 ; et < num_eta_bin[f] ; et++){
+			h1_dpp_v_p_et_bins[f][et] = (TH1F*) Fin[f] -> Get(Form("h1_dpp_v_p_et_bins_%i",et));
+			h1_dth_v_p_et_bins[f][et] = (TH1F*) Fin[f] -> Get(Form("h1_dth_v_p_et_bins_%i",et));
+			h1_dph_v_p_et_bins[f][et] = (TH1F*) Fin[f] -> Get(Form("h1_dph_v_p_et_bins_%i",et));
+		}
+
+		for(int p = 0 ; p < num_mom_bin[f] ; p++){
+			h1_dpp_v_et_p_bins[f][p ] = (TH1F*) Fin[f] -> Get(Form("h1_dpp_v_et_p_bins_%i",p ));
+			h1_dth_v_et_p_bins[f][p ] = (TH1F*) Fin[f] -> Get(Form("h1_dth_v_et_p_bins_%i",p ));
+			h1_dph_v_et_p_bins[f][p ] = (TH1F*) Fin[f] -> Get(Form("h1_dph_v_et_p_bins_%i",p ));
+		}
+	}
+
+	cout << "\neta bin boundaries:\n"; for(int et = 0 ; et < num_eta_bin[0]+1 ; et++) cout << (*TVT_eta_bin[0])[et] << ", "; cout << "\n";
+	cout << "\np bin boundaries:\n"  ; for(int p  = 0 ; p  < num_mom_bin[0]+1 ; p ++) cout << (*TVT_mom_bin[0])[ p] << ", "; cout << "\n\n";
+
+	// #######################################################################################################################################
+        // EDIT THE CODE BELOW DEPENDING ON WHAT YOU WANT TO PLOT
+	// ------------------------------------------------------------------------------
+	TGraphErrors ** g_dpp_v_et_p_bins = new TGraphErrors * [num_mom_bin[0]];
+	TGraphErrors ** g_dth_v_p_et_bins = new TGraphErrors * [num_eta_bin[0]];
+	TGraphErrors ** g_dph_v_p_et_bins = new TGraphErrors * [num_eta_bin[0]];
+	for(int p = 0 ; p < num_mom_bin[0] ; p++){
+		g_dpp_v_et_p_bins[p] = graph_from_histo( h1_dpp_v_et_p_bins[0][p] , 51 + p*5 , 20 , 0 , 999 );
+	}
+
+	for(int et = 14 ; et < num_eta_bin[0] ; et++){
+		g_dth_v_p_et_bins[et] = graph_from_histo( h1_dth_v_p_et_bins[0][et] , 51 + (et-14)*3 , 20 , 0.02 ,10 );
+		g_dph_v_p_et_bins[et] = graph_from_histo( h1_dph_v_p_et_bins[0][et] , 51 + (et-14)*3 , 20 , 0.04 ,60 );
+	}
+
+	// ------------------------------------------------------------------------------
+	// Plotting graphs
+	TCanvas * c1 = new TCanvas("c1","c1",1200,900);
+	c1 -> Divide(2,2);
+	c1 -> cd(1); gPad -> SetRightMargin(0.02); gPad -> SetBottomMargin(0.13); gPad -> SetLeftMargin(0.13);
+	g_dpp_v_et_p_bins[num_mom_bin[0]-1] -> GetXaxis() -> SetRangeUser(0,4);
+	g_dpp_v_et_p_bins[num_mom_bin[0]-1] -> Draw("APL");
+	for(int p = 0 ; p < num_mom_bin[0] ; p++){
+                g_dpp_v_et_p_bins[p] -> Draw("samePL");
+        }
+	
+	c1 -> cd(3); gPad -> SetRightMargin(0.02); gPad -> SetBottomMargin(0.13); gPad -> SetLeftMargin(0.13); gPad -> SetLogy();
+	g_dth_v_p_et_bins[14] -> Draw("APL");
+	for(int et = 14 ; et < num_eta_bin[0] ; et++){
+		g_dth_v_p_et_bins[et] -> Draw("samePL");
+	}
+
+	c1 -> cd(4); gPad -> SetRightMargin(0.02); gPad -> SetBottomMargin(0.13); gPad -> SetLeftMargin(0.13); gPad -> SetLogy();
+        g_dph_v_p_et_bins[14] -> Draw("APL");
+        for(int et = 15 ; et < num_eta_bin[0] ; et++){
+                g_dph_v_p_et_bins[et] -> Draw("samePL");
+        }
+
+	// ------------
+	TLegend * leg1 = new TLegend(0.20,0.25,0.68,0.89);
+	leg1 -> SetLineColor(0);
+	leg1 -> AddEntry( (TObject*)0, "Beast (3.0 T), 10#mum pixel", "");
+	leg1 -> AddEntry( (TObject*)0, "p [ GeV/#it{c} ]", "");
+	for(int p = 0 ; p < num_mom_bin[0] ; p++){
+		leg1 -> AddEntry( g_dpp_v_et_p_bins[p] , Form("( %.0f , %.0f )", (*TVT_mom_bin[0])[p] , (*TVT_mom_bin[0])[p+1] ) );
+	}
+	c1 -> cd(1);
+	leg1 -> Draw("same");
+	// ------------
+	TLegend * leg2 = new TLegend(0.30,0.5,0.95,0.89);
+        leg2 -> SetLineColor(0);
+	leg2 -> SetNColumns(3);
+        leg2 -> AddEntry( (TObject*)0, "#eta", "");
+	leg2 -> AddEntry( (TObject*)0, "" , "");
+	leg2 -> AddEntry( (TObject*)0, "" , "");
+        for(int et = 15 ; et < num_eta_bin[0] ; et++){
+                leg2 -> AddEntry( g_dth_v_p_et_bins[et] , Form("( %.1f , %.1f )", (*TVT_eta_bin[0])[et] , (*TVT_eta_bin[0])[et+1] ) );
+        }
+        c1 -> cd(3);
+        leg2 -> Draw("same");
+	// ------------
+	c1 -> Modified();
+	c1 -> Update();	
+	// ------------------------------------------------------------------------------
+	// Saving results to pdf files
+	c1 -> Print("single_B_field.pdf");
+
+	myapp -> Run();
+	return 0;
+}
+// ============================================================================================================================================
+void prettyTH1F( TH1F * h1 , int color , int marker , float min , float max ){
+	h1 -> SetLineWidth(2);
+	h1 -> SetLineColor(color);
+	h1 -> SetMarkerStyle(marker);
+	h1 -> SetMarkerColor(color);
+
+	if(min!=999) h1 -> SetMinimum(min);
+	if(max!=999) h1 -> SetMaximum(max);
+
+	h1 -> GetXaxis() -> CenterTitle();
+	h1 -> GetXaxis() -> SetNdivisions(108); // to draw less tick marks
+	h1 -> GetYaxis() -> CenterTitle();
+	h1 -> GetYaxis() -> SetNdivisions(108); // to draw less tick marks
+
+	h1 -> SetMinimum(0.001);
+}
+// ============================================================================================================================================
+int idx_from_vector( double value , TVectorT<double> * vec ){
+	int size_vec = (*vec).GetNoElements();
+	double diff = 999.;
+	int idx = -1;
+	for(int i = 0 ; i < size_vec ; i++){
+		if(abs((*vec)[i] - value)<diff){
+			diff = abs((*vec)[i] - value);
+			idx = i;
+		}
+	}
+	return idx;
+}
+// ============================================================================================================================================
+TGraphErrors * graph_from_histo( TH1F * h1 , int color , int marker , float min , float max ){
+	// Gathering information from histogram
+	float yval[500] = {0};
+	float xval[500] = {0};
+	float err [500] = {0};
+	int ctr = 0;
+
+	TString xax = h1 -> GetXaxis() -> GetTitle();
+	TString yax = h1 -> GetYaxis() -> GetTitle();
+	TString tit = h1 -> GetTitle();
+
+	const int nbin = h1 -> GetSize()-2;
+
+	for(int i = 0 ; i < nbin ; i++){
+		if(h1 -> GetBinContent(i+1)>0){
+			yval[ctr] = h1 -> GetBinContent(i+1);
+			xval[ctr] = h1 -> GetXaxis() -> GetBinCenter(i+1);
+			err[ctr] = h1 -> GetBinError(i+1);
+			ctr++;
+		}
+	}
+
+	// Creating a TGraphErrors object that mirrors the histogram
+	TGraphErrors * g_l1 = new TGraphErrors(ctr,xval,yval,0,err);
+	g_l1->SetLineColor(color);
+	g_l1->SetMarkerColor(color);
+	g_l1->SetMarkerStyle(marker);
+	g_l1->SetMarkerSize(1.4);
+	g_l1->SetLineWidth(2);
+
+	g_l1->GetXaxis()->SetTitle(xax);
+	g_l1->GetXaxis()->SetNdivisions(108);
+	g_l1->GetXaxis()->SetTitleSize(0.05);
+	g_l1->GetXaxis()->SetLabelSize(0.05);
+	g_l1->GetXaxis()->CenterTitle();
+
+	g_l1->GetYaxis()->SetTitle(yax);
+	g_l1->GetYaxis()->SetNdivisions(108);
+	g_l1->GetYaxis()->SetTitleSize(0.05);
+	g_l1->GetYaxis()->SetLabelSize(0.05);
+	g_l1->GetYaxis()->CenterTitle();
+
+	g_l1->SetTitle(tit);
+
+	if(min!=999) g_l1 -> SetMinimum(min);
+        if(max!=999) g_l1 -> SetMaximum(max);
+
+	float minxval = h1 -> GetBinLowEdge(1);
+	float maxxval = h1 -> GetBinLowEdge(nbin) + h1 -> GetBinWidth(nbin); 
+
+	g_l1->GetXaxis()->SetRangeUser( minxval , maxxval );
+
+	return g_l1;
+}
