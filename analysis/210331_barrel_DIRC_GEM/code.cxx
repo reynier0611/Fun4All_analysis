41866c342bd2e148f0e39ca58a297a3b519eb8c9
Reynier Cruz Torres <reynier@mit.edu>
2021-04-07 14:16:10 -0700

    Significant editing of the code

--- analysis/210331_barrel_DIRC_GEM/code.cxx
+++ analysis/210331_barrel_DIRC_GEM/code.cxx
@@ -138,6 +138,7 @@ int main(int argc, char ** argv) {
 
 	TGraphErrors *** g_dpp_v_p_et_bins   = new TGraphErrors ** [size_loaded];
 	TGraphErrors *** g_dppT_v_pT_et_bins = new TGraphErrors ** [size_loaded];
+
 	for(int f = 0 ; f < size_loaded ; f++){
 		double max = f%2==0 ? 0.9 : 2;
 		g_dpp_v_p_et_bins  [f] = new TGraphErrors * [num_eta_bin[0]];
@@ -150,6 +151,39 @@ int main(int argc, char ** argv) {
 		}
 	}
 
+	// Ratios (different configurations to all-si tracker standalone)
+	TH1F *** h1_dpp_v_p_et_bins_i_over_0 = new TH1F ** [size_loaded];
+
+	for(int f = 0 ; f < size_loaded ; f++){
+		h1_dpp_v_p_et_bins_i_over_0[f] = new TH1F * [num_eta_bin[0]];
+		int idx = f%2==0 ? 0 : 1;
+		for(int p = 0 ; p < num_eta_bin[f] ; p++){
+			h1_dpp_v_p_et_bins_i_over_0[f][p] = (TH1F*) h1_dpp_v_p_et_bins[f][p] -> Clone();
+			h1_dpp_v_p_et_bins_i_over_0[f][p] -> SetName(Form("h1_dpp_v_p_et_bins_%i_over_0_%i",f,p));
+			h1_dpp_v_p_et_bins_i_over_0[f][p] -> Divide(h1_dpp_v_p_et_bins[idx][p]);
+
+			prettyTH1( h1_dpp_v_p_et_bins_i_over_0[f][p] , color[f] , marker[f] , 0.5 , 1.2 );
+			h1_dpp_v_p_et_bins_i_over_0[f][p] -> GetYaxis() -> SetTitle("ratio to all-si tracker only");
+			h1_dpp_v_p_et_bins_i_over_0[f][p] -> SetTitle(Form("%.1f < |#eta| < %.1f",(*TVT_eta_bin[0])[p],(*TVT_eta_bin[0])[p+1]));
+		}
+	} 
+
+	// Ratios (B=1.4T over B=3.0T)
+	TH1F *** h1_dpp_v_p_et_bins_1_4_over_3_0 = new TH1F ** [size_loaded/2];
+
+	for(int f = 0 ; f < size_loaded/2 ; f++){
+		h1_dpp_v_p_et_bins_1_4_over_3_0[f] = new TH1F * [num_eta_bin[0]];
+		for(int p = 0 ; p < num_eta_bin[f] ; p++){
+			h1_dpp_v_p_et_bins_1_4_over_3_0[f][p] = (TH1F*) h1_dpp_v_p_et_bins[2*f+1][p] -> Clone();
+			h1_dpp_v_p_et_bins_1_4_over_3_0[f][p] -> SetName(Form("h1_dpp_v_p_et_bins_Brat_%i_%i",f,p));
+			h1_dpp_v_p_et_bins_1_4_over_3_0[f][p] -> Divide(h1_dpp_v_p_et_bins[2*f][p]);
+
+			prettyTH1( h1_dpp_v_p_et_bins_1_4_over_3_0[f][p] , color[2*f] , marker[2*f] , 0. , 3.0 );
+			h1_dpp_v_p_et_bins_1_4_over_3_0[f][p] -> GetYaxis() -> SetTitle("ratio (1.4T)/(3.0T)");
+			h1_dpp_v_p_et_bins_1_4_over_3_0[f][p] -> SetTitle(Form("%.1f < |#eta| < %.1f",(*TVT_eta_bin[0])[p],(*TVT_eta_bin[0])[p+1]));
+		}
+	}
+
 	// ------------------------------------------------------------------------------
 	// Legend
 	TLegend * leg1 = new TLegend(0.25,0.18,0.95,0.4);
@@ -158,12 +192,30 @@ int main(int argc, char ** argv) {
                 leg1 -> AddEntry(g_dpp_v_p_et_bins[j][0],labels[j]);
         }
 
-	TLegend * leg2 = new TLegend(0.,0.,1.,1.);
-	leg2 -> SetLineColor(0);
-	for(int j = 0 ; j < size_loaded ; j+=2){	
-		leg2 -> AddEntry(g_dpp_v_p_et_bins[j][0],labels[j]);
-	}        
+	TLegend * leg2 = new TLegend(0.25,0.18,0.95,0.4);
+        leg2 -> SetLineColor(0);
+        for(int j = 1 ; j < size_loaded ; j+=2){
+                leg2 -> AddEntry(g_dpp_v_p_et_bins[j][0],labels[j]);
+        }
 
+	TLegend * leg3 = new TLegend(0.,0.,1.,1.);
+	leg3 -> SetLineColor(0);
+	for(int j = 0 ; j < size_loaded ; j+=2){	
+		leg3 -> AddEntry(g_dpp_v_p_et_bins[j][0],labels[j]);
+	}
+
+	TLegend * leg4 = new TLegend(0.,0.,1.,1.);
+        leg4 -> SetLineColor(0);
+        for(int j = 1 ; j < size_loaded ; j+=2){
+                leg4 -> AddEntry(g_dpp_v_p_et_bins[j][0],labels[j]);
+        }
+	// ------------------------------------------------------------------------------
+        // Physics Working Group Requirements
+        // https://docs.google.com/spreadsheets/d/1ynU7Cu7NlwRvMtbtFdlp_B5xXkw8yBAtWJbenMf-P3U/edit#gid=368031287
+        // https://wiki.bnl.gov/eicug/index.php/Yellow_Report_Physics_Common
+        TF1 * f_PWG_req_eta_lt_1 = new TF1("f_PWG_req_eta_lt_1","sqrt(pow(0.05*x,2)+pow(0.5,2))",0,30);
+	f_PWG_req_eta_lt_1 -> SetLineColor(11);
+	f_PWG_req_eta_lt_1 -> SetLineStyle(2);
 	// ------------------------------------------------------------------------------
 	// Plotting graphs
 	TCanvas * c0 = new TCanvas("c0","c0",1400,900);
@@ -173,42 +225,102 @@ int main(int argc, char ** argv) {
                 g_dpp_v_p_et_bins[j][0] -> Draw("samePE3");
         }
         leg1 -> Draw("same");
+	f_PWG_req_eta_lt_1 -> Draw("same");
         c0 -> Modified();
         c0 -> Update();
 	// ----------------------------------
 	TCanvas * c1 = new TCanvas("c1","c1",1400,900);
-	c1 -> Divide(3,2);
+        gPad -> SetRightMargin(0.044); gPad -> SetLeftMargin(0.14); gPad -> SetBottomMargin(0.17);
+        g_dpp_v_p_et_bins[1][0] -> Draw("APE3");
+        for(int j = 3 ; j < size_loaded ; j+=2){
+                g_dpp_v_p_et_bins[j][0] -> Draw("samePE3");
+        }
+        leg2 -> Draw("same");
+        f_PWG_req_eta_lt_1 -> Draw("same");
+        c1 -> Modified();
+        c1 -> Update();
+	// ----------------------------------
+	TCanvas * c2 = new TCanvas("c2","c2",1400,900);
+	c2 -> Divide(3,2);
 	for(int i = 0 ; i < num_eta_bin[0]-1 ; i++){
-		c1 -> cd(i+1); gPad -> SetRightMargin(0.044); gPad -> SetLeftMargin(0.19); gPad -> SetBottomMargin(0.17);	
+		c2 -> cd(i+1); gPad -> SetRightMargin(0.044); gPad -> SetLeftMargin(0.19); gPad -> SetBottomMargin(0.17);	
 		g_dpp_v_p_et_bins[0][i] -> Draw("APE3");
 		for(int j = 2 ; j < size_loaded ; j+=2){
 			g_dpp_v_p_et_bins[j][i] -> Draw("samePE3");
 		}
+		f_PWG_req_eta_lt_1 -> Draw("same");
 	}
-	c1 -> cd(6);
-	leg2 -> Draw("same");
-	c1 -> Modified();
-	c1 -> Update();
+	c2 -> cd(6);
+	leg3 -> Draw("same");
+	c2 -> Modified();
+	c2 -> Update();
 	// ----------------------------------
-	TCanvas * c2 = new TCanvas("c2","c2",1400,900);
-        c2 -> Divide(3,2);
+	TCanvas * c3 = new TCanvas("c3","c3",1400,900);
+        c3 -> Divide(3,2);
         for(int i = 0 ; i < num_eta_bin[0]-1 ; i++){
-                c2 -> cd(i+1); gPad -> SetRightMargin(0.044); gPad -> SetLeftMargin(0.19); gPad -> SetBottomMargin(0.17); 
+                c3 -> cd(i+1); gPad -> SetRightMargin(0.044); gPad -> SetLeftMargin(0.19); gPad -> SetBottomMargin(0.17); 
                 g_dpp_v_p_et_bins[1][i] -> Draw("APE3");
                 for(int j = 3 ; j < size_loaded ; j+=2){
                         g_dpp_v_p_et_bins[j][i] -> Draw("samePE3");
                 }
+		f_PWG_req_eta_lt_1 -> Draw("same");
         }
-        c2 -> cd(6);
-        leg2 -> Draw("same");
-        c2 -> Modified();
-        c2 -> Update();
+        c3 -> cd(6);
+        leg4 -> Draw("same");
+        c3 -> Modified();
+        c3 -> Update();
+	// ----------------------------------
+	TCanvas * c4 = new TCanvas("c4","c4",1400,900);
+        c4 -> Divide(3,2);
+	for(int i = 0 ; i < num_eta_bin[0]-1 ; i++){
+                c4 -> cd(i+1); gPad -> SetRightMargin(0.044); gPad -> SetLeftMargin(0.19); gPad -> SetBottomMargin(0.17);
+                h1_dpp_v_p_et_bins_i_over_0[0][i] -> Draw();
+                for(int j = 2 ; j < size_loaded ; j+=2){
+                        h1_dpp_v_p_et_bins_i_over_0[j][i] -> Draw("same");
+                }
+        }
+        c4 -> cd(6);
+        leg3 -> Draw("same");
+	c4 -> Modified();
+        c4 -> Update();
+	// ----------------------------------
+	TCanvas * c5 = new TCanvas("c5","c5",1400,900);
+        c5 -> Divide(3,2);
+        for(int i = 0 ; i < num_eta_bin[0]-1 ; i++){
+                c5 -> cd(i+1); gPad -> SetRightMargin(0.044); gPad -> SetLeftMargin(0.19); gPad -> SetBottomMargin(0.17);
+                h1_dpp_v_p_et_bins_i_over_0[1][i] -> Draw();
+                for(int j = 3 ; j < size_loaded ; j+=2){
+                        h1_dpp_v_p_et_bins_i_over_0[j][i] -> Draw("same");
+                }
+        }
+        c5 -> cd(6);
+        leg4 -> Draw("same");
+	c5 -> Modified();
+        c5 -> Update();
+	// ----------------------------------
+        TCanvas * c6 = new TCanvas("c6","c6",1400,900);
+        c6 -> Divide(3,2);
+	for(int i = 0 ; i < num_eta_bin[0]-1 ; i++){
+                c6 -> cd(i+1); gPad -> SetRightMargin(0.044); gPad -> SetLeftMargin(0.19); gPad -> SetBottomMargin(0.17);
+		h1_dpp_v_p_et_bins_1_4_over_3_0[0][i] -> Draw();
+                for(int j = 0 ; j < size_loaded/2 ; j++){
+			h1_dpp_v_p_et_bins_1_4_over_3_0[j][i] -> Draw("same");
+		}
+	}
+	c6 -> cd(6);
+	leg3 -> Draw("same");
+	c6 -> Modified();
+        c6 -> Update();
 	// ------------------------------------------------------------------------------
 	// Saving results to pdf files
 	TString out_pdf_name = "results_code.pdf";
 	c0 -> Print(out_pdf_name+"(");
 	c1 -> Print(out_pdf_name    );
-	c2 -> Print(out_pdf_name+")");
+	c2 -> Print(out_pdf_name    );
+	c3 -> Print(out_pdf_name    );
+	c4 -> Print(out_pdf_name    );
+	c5 -> Print(out_pdf_name    );
+	c6 -> Print(out_pdf_name+")");
 
 	myapp -> Run();
 	return 0;
@@ -260,7 +372,7 @@ void prettyTH1( TH1F * h1 , int color , int marker , float min , float max ){
 	h1->GetYaxis()->SetTitleSize(0.07);
 	h1->GetYaxis()->SetLabelSize(0.07);
 	h1->GetYaxis()->CenterTitle();
-	h1->GetYaxis()->SetTitleOffset(1);
+	h1->GetYaxis()->SetTitleOffset(1.2);
 
 	if(min!=999) h1 -> SetMinimum(min);
         if(max!=999) h1 -> SetMaximum(max);
@@ -308,7 +420,7 @@ TGraphErrors * graph_from_histo( TH1F * h1 , int color , int marker , float min 
         g_l1->GetYaxis()->SetTitleSize(0.07);
         g_l1->GetYaxis()->SetLabelSize(0.07);
         g_l1->GetYaxis()->CenterTitle();
-	g_l1->GetYaxis()->SetTitleOffset(1);
+	g_l1->GetYaxis()->SetTitleOffset(1.);
 
         g_l1->SetTitle(tit);
 
